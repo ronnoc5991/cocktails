f77ee04847e97dbcae9b553acc1921ff464a5ceb
Connor Streng <cstreng95@gmail.com>
2023-02-15 20:46:45 +0100

    Add useData hook

---
+++ src/hooks/useData.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function useData<ResponseType>(
+  url: string | null
+): ResponseType | null {
+  const [data, setData] = useState<ResponseType | null>(null);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const abortController = new AbortController();
+
+    fetch(url, { signal: abortController.signal })
+      .then((response) => {
+        if (response.ok) return response.json();
+        return Promise.reject(response);
+      })
+      .then((data) => setData(data))
+      .catch((error) => {
+        if (abortController.signal.aborted) return;
+        console.error(error.status);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url]);
+
+  return data;
+}
