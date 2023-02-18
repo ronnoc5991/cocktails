4bf76e2f7ef3e80bc49a92dece6dd9c46453f15c
Connor Streng <cstreng95@gmail.com>
2023-02-18 21:37:16 +0100

    Use ContentSection in DrinkBasicInfo

--- src/components/organisms/DrinkBasicInfo/DrinkBasicInfo.tsx
+++ src/components/organisms/DrinkBasicInfo/DrinkBasicInfo.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { BaseProps } from "../../../types/BaseProps";
 import Heading, { HeadingProps } from "../../atoms/Heading/Heading";
 import Text from "../../atoms/Text/Text";
+import ContentSection from "../../molecules/ContentSection/ContentSection";
 
 export type DrinkBasicInfoProps = BaseProps & {
   name: string;
@@ -18,21 +19,24 @@ function DrinkBasicInfo({
   className,
 }: DrinkBasicInfoProps) {
   return (
-    <div className={classNames(className)}>
-      <Heading as={nameAs} size="large" variant="bold">
-        {name}
-      </Heading>
-      {category && (
-        <Text as="span" variant="normal-italic">
-          {category}
-        </Text>
-      )}
-      {alcoholic && (
-        <Text as="span" variant="normal-italic">
-          {alcoholic}
-        </Text>
-      )}
-    </div>
+    <ContentSection
+      title={
+        <Heading as={nameAs} size="large" variant="bold">
+          {name}
+        </Heading>
+      }
+      body={
+        <>
+          <Text as="span" variant="normal-italic">
+            {category}
+          </Text>
+          <Text as="span" variant="normal-italic">
+            {alcoholic}
+          </Text>
+        </>
+      }
+      className={classNames(className)}
+    />
   );
 }
 
