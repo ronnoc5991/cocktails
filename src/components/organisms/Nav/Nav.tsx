3467e8b2a2877ea99a7ffaa026c665539138baa2
Connor Streng <cstreng95@gmail.com>
2023-02-18 16:43:59 +0100

    Make Nav more general by removing context

--- src/components/organisms/Nav/Nav.tsx
+++ src/components/organisms/Nav/Nav.tsx
@@ -1,30 +1,26 @@
 import classNames from "classnames";
 import { Link } from "react-router-dom";
-import paths from "../../../paths";
 import { BaseProps } from "../../../types/BaseProps";
 import Text from "../../atoms/Text/Text";
 import "./styles.scss";
 
-type Props = BaseProps & {};
+export type NavProps = BaseProps & {
+  items: Array<{ path: string; label: string }>;
+};
 
-function Nav({ className }: Props) {
+function Nav({ items, className }: NavProps) {
   return (
     <nav className={classNames("Nav", className)}>
       <ul>
-        <li>
-          <Link to={paths.home}>
-            <Text as="span" size="large">
-              Home
-            </Text>
-          </Link>
-        </li>
-        <li>
-          <Link to={paths["drink-search"]}>
-            <Text as="span" size="large">
-              Search for Drinks
-            </Text>
-          </Link>
-        </li>
+        {items.map((item) => (
+          <li key={item.label}>
+            <Link to={item.path}>
+              <Text as="span" size="medium">
+                {item.label}
+              </Text>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
