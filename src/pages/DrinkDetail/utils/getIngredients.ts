1f5df67162a88aa3fe657aadd9f089a89ee84197
Connor Streng <cstreng95@gmail.com>
2023-02-19 09:56:26 +0100

    Leave comment in getIngredients util

--- src/pages/DrinkDetail/utils/getIngredients.ts
+++ src/pages/DrinkDetail/utils/getIngredients.ts
@@ -4,7 +4,8 @@ import { Ingredient } from "../../../types/Ingredient";
 export function getIngredients(drink: Drink): Array<Ingredient> {
   const list: Array<Ingredient> = [];
 
-  // TODO: leave a comment here explaining these numbers?
+  // Drinks can have up to 15 ingredients and corresponding measurements (numbered 1 through 15)
+  // following the naming convention "strIngredient[number]" and "strMeasure[number]"
   for (let i = 1; i <= 15; i++) {
     const nameKey = `strIngredient${i}` as keyof Drink;
     const name = drink[nameKey];
